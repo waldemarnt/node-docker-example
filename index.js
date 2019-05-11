74471aa2df5cdb4189b5eb2db5f9751831c171fa
Waldemar Neto <waldemarnt@gmail.com>
2019-05-11 19:22:36 +1000

    Removing unused code and adding config to index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const log = require('./src/log');
 const { setupApp } = require('./src/app');
-const config = require('./config/environment');
+const config = require('config');
 
 setupApp()
-  .then(app => app.listen(config.port, () => log.info(`app running on port ${config.port}`)))
+  .then(app => app.listen(config.get('App.port'), () => log.info(`app running on port ${config.get('App.port')}`)))
   .catch(error => {
     log.error(error);
     process.exit(1); // eslint-disable-line
