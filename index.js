4886f6dce6b588b27c9c6f07330a65e2deb0cf02
Waldemar Neto <waldemarnt@gmail.com>
2019-07-27 13:21:43 +1000

    Disable lightship due to issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const log = require('./src/log');
 const { setupApp } = require('./src/app');
 const config = require('config');
-const { createLightship } = require('lightship');
+//const { createLightship } = require('lightship');
 const cluster = require('cluster');
 const CPUs = require('os').cpus().length;
 
 log.info('CPUs available', CPUs);
 
-const ls = createLightship();
+//const ls = createLightship();
 
 if(config.get('App.cluster.enabled') && cluster.isMaster) {
   log.info(`Master ${process.pid} is running`);
@@ -25,7 +25,7 @@ if(config.get('App.cluster.enabled') && cluster.isMaster) {
       log.info(`Worker ${process.pid} started`);
       return app;
     })
-    .then(() => ls.signalReady())
+    //.then(() => ls.signalReady())
     .catch(error => {
       log.error(error);
       process.exit(1); // eslint-disable-line
