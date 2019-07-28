a85e52c54724fc312aba38e4010db6dcade7bffe
Waldemar Neto <waldemarnt@gmail.com>
2019-07-28 12:21:21 +1000

    Add shared memory example

--- src/routes/threads.js
+++ src/routes/threads.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fb = require('fibonacci');
 const runFibonacci = require('../workers/fibonacciWorker');
 const runFibonacciPool = require('../workers/fibonacciWorkerPool');
+const runFibonacciShared = require('../workers/fibonacciWorkerShared');
 const log = require('../log');
 
 const router = express.Router();
@@ -18,6 +19,17 @@ router.get('/fibonacciThreaded', async (req, res) => {
 
 router.get('/fibonacciThreadedPool', async (req, res) => {
   runFibonacciPool({ iterations: 10000 }).then(result => log.info(result));
+  res.send('processing');
+});
+
+router.get('/fibonacciThreadedShared', async (req, res) => {
+  /**
+   * Uses shared SharedArrayBuffer to share data with the workers
+   */
+  const sharedUint8Array = new Uint8Array(new SharedArrayBuffer(4));
+  for(let i= 0; i< 4; i++) {
+    runFibonacciShared({ iterations: 1000, position: i, arr: sharedUint8Array }).then(result => console.log(result));
+  }
   res.send('processing');
 });
 
