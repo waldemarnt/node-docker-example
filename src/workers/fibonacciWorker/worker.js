1633a70d968de48981c8100ded5db6aedf5fef6d
Waldemar Neto <waldemarnt@gmail.com>
2019-07-31 23:00:57 +1000

    Change architecture

--- src/workers/fibonacciWorker/worker.js
+++
@@ -1,8 +0,0 @@
-const fb = require('fibonacci');
-const { parentPort, workerData } = require('worker_threads');
-
-const result = fb.iterate(workerData.iterations);
-/**
- * Send a copy the result object back to the main Thread
- */
-parentPort.postMessage(result);
